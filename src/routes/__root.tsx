fad91a6ad341ab4c9e30e251257fd2ea84f6b55d
Nhat Minh (Himemiya Taiyou) <nhatminhwork1996@gmail.com>
2025-05-08 11:33:40 +0700

    feat: init very basic tanstack simple repo

---
+++ src/routes/__root.tsx
@@ -0,0 +1,55 @@
+import type { ReactNode } from "react";
+import {
+  Outlet,
+  createRootRoute,
+  HeadContent,
+  Scripts,
+} from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "Tanstack React Simple",
+      },
+      {
+        name: "description",
+        content: "A simple app using React Start",
+      },
+    ],
+  }),
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+type RootDocumentProps = Readonly<{
+  children: ReactNode;
+}>;
+
+function RootDocument({ children }: RootDocumentProps) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,49 @@
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import fs from "fs";
+
+const filePath = "count.txt";
+
+async function readCount() {
+  return parseInt(
+    await fs.promises.readFile(filePath, "utf-8").catch(() => "0")
+  );
+}
+
+const getCount = createServerFn({
+  method: "GET",
+}).handler(() => {
+  return readCount();
+});
+
+const updateCount = createServerFn({
+  method: "POST",
+})
+  .validator((d: number) => d)
+  .handler(async ({ data }) => {
+    const count = await readCount();
+    return fs.promises.writeFile(filePath, (count + data).toString());
+  });
+
+export const Route = createFileRoute("/")({
+  component: Home,
+  loader: async () => await getCount(),
+});
+
+function Home() {
+  const router = useRouter();
+  const state = Route.useLoaderData();
+
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        updateCount({ data: 1 }).then(() => {
+          router.invalidate();
+        });
+      }}
+    >
+      Add 1 to {state}?
+    </button>
+  );
+}
